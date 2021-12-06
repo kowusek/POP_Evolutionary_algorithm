24ba0178076852cff8f2e66c839c42557bb0da11
kowusek <kowusek@gmail.com>
2021-12-06 20:25:13 +0100

    Added Consts and changed gene class

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+EDGE_COUNT = 12
+DEMAND_COUNT = 66
+DEMAND_PATHS = np.zeros([])
+DEMNADS = np.zeros([])
+PATH_COUNT = 2
+MODULARITY = 0
 
 def main():
     print("In main method")
