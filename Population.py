7c9e1269940cf336425cb71bd2456074ac3cb5b4
dandon223 <daniel.gorniak@op.pl>
2021-12-06 22:00:13 +0100

    parser

--- Population.py
+++ Population.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from Gene import Gene
+from xml.dom.minidom import parse
 
 class Population:
     def __init__(self, popSize: int, mutationProb: int, crossProb: int, pathCount: int, modularity: int, iterCount: int) -> None:
@@ -16,7 +17,35 @@ class Population:
         self.initPopulation()
 
     def loadData(self) -> None:
-        pass
+        document = parse("graf.xml")
+        root = document.documentElement
+        cities = document.getElementsByTagName("node")
+    
+        for city in cities:
+            self.nodes.append(city.getAttribute('id'))
+
+        links = document.getElementsByTagName("link")
+    
+        for link in links:
+            source = link.getElementsByTagName('source')[0].firstChild.nodeValue
+            target = link.getElementsByTagName('target')[0].firstChild.nodeValue
+            self.links_dict[link.getAttribute("id")] = (source,target)
+    
+        demands = document.getElementsByTagName("demand")
+        for demand in demands:
+            source = demand.getElementsByTagName('source')[0].firstChild.nodeValue
+            target = demand.getElementsByTagName('target')[0].firstChild.nodeValue
+            demandValue = demand.getElementsByTagName('demandValue')[0].firstChild.nodeValue
+            self.demands_dict[(source,target)] = demandValue
+        
+            admissiblePaths = demand.getElementsByTagName('admissiblePath')
+            paths = []
+            for admissiblePath in admissiblePaths:
+                path = []
+                for link in admissiblePath.getElementsByTagName('linkId'):
+                    path.append(link.firstChild.nodeValue)
+                paths.append(path)
+            self.paths_dict[(source,target)] =paths
 
     def cross(self, gene) -> tuple[Gene, Gene]:
         pass
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from xml.dom.minidom import parse, parseString
 import numpy as np
 
 EDGE_COUNT = 12
@@ -8,53 +7,10 @@ DEMNADS = np.zeros([])
 PATH_COUNT = 2
 MODULARITY = 0
 
-nodes = []
-links_dict = {}
-demands_dict = {}
-paths_dict = {}
 
 def main():
-    parseXMLFile("graf.xml")
+    print("Czesc")
 
-    print(nodes)
-    print("")
-    print(links_dict)
-    print("")
-    print(demands_dict)
-    print("")
-    print(paths_dict)
-    print("")
-def parseXMLFile(fileName):
-
-    document = parse("graf.xml")
-    root = document.documentElement
-    cities = document.getElementsByTagName("node")
-    
-    for city in cities:
-        nodes.append(city.getAttribute('id'))
-
-    links = document.getElementsByTagName("link")
-    
-    for link in links:
-        source = link.getElementsByTagName('source')[0].firstChild.nodeValue
-        target = link.getElementsByTagName('target')[0].firstChild.nodeValue
-        links_dict[link.getAttribute("id")] = (source,target)
-    
-    demands = document.getElementsByTagName("demand")
-    for demand in demands:
-        source = demand.getElementsByTagName('source')[0].firstChild.nodeValue
-        target = demand.getElementsByTagName('target')[0].firstChild.nodeValue
-        demandValue = demand.getElementsByTagName('demandValue')[0].firstChild.nodeValue
-        demands_dict[(source,target)] = demandValue
-        
-        admissiblePaths = demand.getElementsByTagName('admissiblePath')
-        paths = []
-        for admissiblePath in admissiblePaths:
-            path = []
-            for link in admissiblePath.getElementsByTagName('linkId'):
-                path.append(link.firstChild.nodeValue)
-            paths.append(path)
-        paths_dict[(source,target)] =paths
 
 if __name__=="__main__":
     main()
